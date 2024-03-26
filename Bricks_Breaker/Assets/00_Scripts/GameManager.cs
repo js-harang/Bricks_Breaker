e9b632e1670bc505f4c3f30a9bc5e758c83b45f2
js-harang <87293890+js-harang@users.noreply.github.com>
2024-03-26 17:45:03 +0900

    2024-03-26

--- Bricks_Breaker/Assets/00_Scripts/GameManager.cs
+++ Bricks_Breaker/Assets/00_Scripts/GameManager.cs
@@ -1,44 +1,65 @@
+using DG.Tweening;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    Ball ball;
-    GameObject pausePopup;
-
-    bool isPausePopup = false;
-
-    public TMP_Text lifeTxt;
-
-    [Space]
-
-    public int life = 3;
-    public int score = 0;
-    public bool isPlaying = false;
+    public GameObject loading_UI;
+    public TMP_Text loadingText;
+    public CanvasGroup fadeImg;
+    float fadeDuration = 1.0f;
 
     [Space]
 
     public Material[] bricksColor = new Material[7];
 
+
+    [SerializeField] Slider loadingBar;
+
+    //Ball ball;
+    GameObject pausePopup;
+
+
+
+    bool isPausePopup = false;
+    //public TMP_Text lifeTxt;
+
+    //[Space]
+
+    //public int life = 3;
+    //public int score = 0;
+    //public bool isPlaying = false;
+
     private void Start()
     {
-        ball = GameObject.Find("Ball").GetComponent<Ball>();
         pausePopup = GameObject.Find("Canvas").transform.Find("Pause").gameObject;
+
     }
 
     private void Update()
     {
-        if (SceneManager.GetActiveScene().name == "Mains")
+        //if (SceneManager.GetActiveScene().name == "02_Main")
+        //{
+        //lifeTxt.text = $"Life : {life}\nScore : {score}";
+        //}
+
+        if (SceneManager.GetActiveScene().name == "01_Start")
         {
-            lifeTxt.text = $"Life : {life}\nScore : {score}";
-
-            if (Input.GetKeyDown(KeyCode.Space))
-                GameStart();
-
             if (Input.GetKeyDown(KeyCode.Escape))
-                Pause();
+                EndGame();
         }
+
+        //if (SceneManager.GetActiveScene().name == "02_Main")
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //        GameStart();
+
+        //    if (Input.GetKeyDown(KeyCode.Escape))
+        //        Pause();
+        //}
     }
 
     private void Awake()
@@ -46,41 +67,45 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    #region // 벽돌 색상 설정
-    public Material ColorSetting()
-    {
-        int num = Random.Range(0, 6);
-
-        return bricksColor[num];
-    }
-    #endregion
-
-    public void GameStart()
-    {
-        if (!isPlaying)
-        {
-            ball.movePos = new Vector3(1f, 1f, 0f).normalized;
-            isPlaying = true;
-        }
-    }
-
-    public void Pause()
-    {
-        isPlaying = !isPlaying;
-        isPausePopup = !isPausePopup;
-        pausePopup.SetActive(isPausePopup);
-    }
-
-    public void Continue()
-    {
-        isPlaying = !isPlaying;
-        isPausePopup = !isPausePopup;
-        pausePopup.SetActive(isPausePopup);
-    }
-
     public void StartBtn()
     {
+        ChangeScene("02_Main");
+    }
 
+    private void ChangeScene(string scene)
+    {
+        fadeImg.DOFade(1, fadeDuration).OnStart(() => { fadeImg.blocksRaycasts = true; }).OnComplete(() => { StartCoroutine(LoadScene("02_Main")); });
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        loading_UI.SetActive(true);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+
+        float timer = 0;
+
+        while (!(async.isDone))
+        {
+            yield return null;
+
+            if (async.progress < 0.9f)
+                loadingBar.value = async.progress;
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                loadingBar.value = Mathf.Lerp(0.9f, 1f, timer);
+
+                if (loadingBar.value >= 1f)
+                {
+                    async.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+
+            loadingText.text = $"{loadingBar.value * 100:F0} %";
+        }
     }
 
     public void ScoreBtn()
@@ -93,6 +118,14 @@ public class GameManager : MonoBehaviour
 
     }
 
+    #region // 벽돌 색상 설정
+    public Material ColorSetting()
+    {
+        int num = Random.Range(0, 6);
+
+        return bricksColor[num];
+    }
+    #endregion
     public void Exit()
     {
         #region
